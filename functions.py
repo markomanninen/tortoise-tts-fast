54a0402d6c94948790640ec29e09940b9609696b
Marko Manninen <elonmedia@gmail.com>
2023-03-01 13:47:30 +0200

    added check gpu function

--- functions.py
+++ functions.py
@@ -38,6 +38,25 @@ except:
     from ipywidgets import FileUpload
     from IPython.display import display
 
+from numba import cuda
+
+def check_gpu():
+    cc_cores_per_SM_dict = {(2,0) : 32, (2,1) : 48, (3,0) : 192, (3,5) : 192, (3,7) : 192, (5,0) : 128, (5,2) : 128, (6,0) : 64, (6,1) : 128, (7,0) : 64, (7,5) : 64, (8,0) : 64, (8,6) : 128, (8,9) : 128, (9,0) : 128 }
+    # the above dictionary should result in a value of "None" if a cc match 
+    # is not found.  The dictionary needs to be extended as new devices become
+    # available, and currently does not account for all Jetson devices
+    try:
+      device = cuda.get_current_device()
+      my_sms = getattr(device, 'MULTIPROCESSOR_COUNT')
+      my_cc = device.compute_capability
+      cores_per_sm = cc_cores_per_SM_dict.get(my_cc)
+      total_cores = cores_per_sm*my_sms
+      print("GPU compute capability: " , my_cc)
+      print("GPU total number of SMs: " , my_sms)
+      print("total cores: " , total_cores)
+    except:
+      print("GPU not available!")
+
 def remove_old_files(path):
     for f in glob(path):
         try:
